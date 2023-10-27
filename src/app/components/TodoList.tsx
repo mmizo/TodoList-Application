e11e3e19c8cbd80c0f2f361b62b4eaa98a4763c6
hamzaKh11 <hamza.khalid@edu.uca.ma>
2023-10-27 19:49:59 +0100

    Resolved merge conflicts

--- src/app/components/TodoList.tsx
+++ src/app/components/TodoList.tsx
@@ -29,16 +29,26 @@ const TodoList: React.FC = () => {
   const prevTasksRef = useRef<Task[]>([]);
 
   useEffect(() => {
-    // Load tasks from IndexedDB
-    get(TASKS_STORAGE_KEY).then((savedTasks: Task[] | undefined) => {
-      if (savedTasks) {
-        console.log("Loaded tasks from IndexedDB:", savedTasks);
-        setTasks(savedTasks);
-      } else {
-        console.log("No tasks found in IndexedDB.");
-      }
-    });
+    const savedTasks = JSON.parse(
+      localStorage.getItem(TASKS_STORAGE_KEY) || "[]"
+    );
+    if (savedTasks.length > 0) {
+      console.log("Loaded tasks from LocalStorage:", savedTasks);
+      setTasks(savedTasks);
+    } else {
+      console.log("No tasks found in LocalStorage.");
+    }
   }, []);
+
+  useEffect(() => {
+    if (
+      tasks.length > 0 ||
+      (tasks.length === 0 && prevTasksRef.current.length > 0)
+    ) {
+      localStorage.setItem(TASKS_STORAGE_KEY, JSON.stringify(tasks));
+      console.log("Saved tasks to LocalStorage:", tasks);
+    }
+  }, [tasks]);
 
   useEffect(() => {
     // Update the ref to the current tasks
@@ -48,18 +58,6 @@ const TodoList: React.FC = () => {
   const capitalizeFirstLetter = (string: string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
   };
-
-  useEffect(() => {
-    if (
-      tasks.length > 0 ||
-      (tasks.length === 0 && prevTasksRef.current.length > 0)
-    ) {
-      // Save tasks to IndexedDB
-      set(TASKS_STORAGE_KEY, tasks).then(() => {
-        console.log("Saved tasks to IndexedDB:", tasks);
-      });
-    }
-  }, [tasks]);
 
   const addTask = () => {
     if (taskText.trim()) {
